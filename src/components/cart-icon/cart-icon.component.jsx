b9b4f80677d2c07ec22dedda3d2dd1792af5095c
Aaron <aaronchou@elandfintech.com>
2019-12-20 17:15:10 +0800

    update redering of components by using styled component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
-import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 
-import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
-import './cart-icon.styles.scss';
+import "./cart-icon.styles.scss";
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => (
-  <div className='cart-icon' onClick={toggleCartHidden}>
-    <ShoppingIcon className='shopping-icon' />
-<span className='item-count'>{itemCount}</span>
+  <div className="cart-icon" onClick={toggleCartHidden}>
+    <ShoppingIcon className="shopping-icon" />
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 
@@ -24,7 +24,4 @@ const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
